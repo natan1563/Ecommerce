ef2225df91849bae32267645e15453122bfe85b4
natan1563 <natromafe@gmail.com>
2021-02-17 09:51:32 -0300

    Criada lógica para recuperação de senha enviando um link via e-mail

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,11 +4,13 @@ namespace Hcode\Model;
 
 use \Hcode\Model;
 use \Hcode\DB\Sql;
+use Hcode\Mailer;
 
 class User extends Model
 {
 
     const SESSION = "User";
+    const SECRET = "Hcodephp7_Secret";
 
     public static function login($login, $password)
     {
@@ -125,6 +127,113 @@ class User extends Model
         $sql = new Sql;
 
         $sql->query("CALL sp_users_delete(:iduser)", [
+            ":iduser" => $this->getiduser()
+        ]);
+    }
+
+    public static function getForgot($email, $inadmin = true)
+    {
+
+        $sql = new Sql;
+
+        $results = $sql->select("SELECT * FROM tb_persons INNER JOIN tb_users ON tb_persons.idperson = tb_users.iduser WHERE desemail = :email", [
+            ":email" => $email
+        ]);
+
+
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possivel recuperar a senha.");
+        } else {
+
+            $data = $results[0];
+
+            $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", 
+            [
+                ":iduser" => $data["iduser"],
+                ":desip" => $_SERVER["REMOTE_ADDR"]
+            ]);
+            
+           
+            if(empty($results)) {
+
+                throw new \Exception("Não foi possível recuperar a senha.");
+
+            } else {
+                
+                $dataRecovery = $results2[0];
+
+                $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+
+                $code = openssl_encrypt($dataRecovery["idrecovery"], "aes-256-cbc", User::SECRET, 0, $iv);
+                $result = base64_encode($iv.$code);
+
+                $link = ($inadmin === true ) ? 
+                    "http://www.natecnologia.com.br/admin/forgot/reset?code={$result}" : 
+                    "http://www.natecnologia.com.br/forgot/reset?code={$result}";
+                
+
+                $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha - NaTecnologias", "forgot", [
+                    "name" => $data["desperson"],
+                    "link" => $link
+                ]);
+
+                $mailer->send();
+               
+                return $data;
+
+            }
+        }
+    }
+
+    public static function validForgotDecrypt($result)
+    {
+        $result = base64_decode($result);
+        $code = mb_substr($result, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
+        $iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit'); 
+        $idrecovery = openssl_decrypt($code, "aes-256-cbc", User::SECRET, 0, $iv);
+        
+        $sql = new Sql();
+        
+        $results = $sql->select(
+                        'SELECT * FROM tb_userspasswordsrecoveries AS a 
+                        INNER JOIN tb_users AS b USING(iduser) 
+                        INNER JOIN tb_persons AS c USING(idperson) 
+                        WHERE 
+                            a.idrecovery = :idrecovery 
+                            AND 
+                            a.dtrecovery IS NULL
+                            AND
+                            DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+                            ',
+                            [
+                                ":idrecovery" => $idrecovery
+                            ]
+                    );
+        
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possivel recuperar a senha");
+        } else {
+            return $results[0];
+        }
+
+    }
+
+    public static function setForgotUsed($idrecovery) 
+    {
+        $sql = new Sql();
+        
+        $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", [
+            ":idrecovery" => $idrecovery
+        ]);
+    }
+
+    public function setPassword($password)
+    {
+        
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", [
+            ":password" => $password,
             ":iduser" => $this->getiduser()
         ]);
     }
